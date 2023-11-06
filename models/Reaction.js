4fd4d0b5b78ef7c0ade07e61cf5056194f91a60b
dansim6935 <dansim6935@gmail.com>
2023-11-05 18:50:04 -0800

    Added Reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max: [280, 'Uh-oh, too many characters!'],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    }
+  },
+  {
+    toJSON: {
+      // virtuals:true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
